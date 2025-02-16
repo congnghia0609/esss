02e2a9aa1409c1cdb1896191a245d26e841523b7
congnghia0609 <congnghia0609@gmail.com>
2025-02-17 03:57:17 +0700

    init project

---
+++ lib/esss.ex
@@ -0,0 +1,18 @@
+defmodule ESSS do
+  @moduledoc """
+  Documentation for `ESSS`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ESSS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/esss_test.exs
@@ -0,0 +1,8 @@
+defmodule ESSSTest do
+  use ExUnit.Case
+  doctest ESSS
+
+  test "greets the world" do
+    assert ESSS.hello() == :world
+  end
+end
