1f717007649183e827a12f154a6cf6b214c7b9a1
congnghia0609 <congnghia0609@gmail.com>
2025-02-17 07:48:57 +0700

    add code convert int to hex

--- lib/esss.ex
+++ lib/esss.ex
@@ -1,18 +1,65 @@
+# @author nghiatc
+# @since Feb 17, 2025
+
 defmodule ESSS do
   @moduledoc """
   Documentation for `ESSS`.
   """
 
+  # @doc """
+  # Hello world.
+
+  # ## Examples
+
+  #     iex> ESSS.hello()
+  #     :world
+
+  # """
+  # def hello do
+  #   :world
+  # end
+
+  # The largest PRIME 256-bit
+  # https://primes.utm.edu/lists/2small/200bit.html
+  # prime = 2^n - k = 2^256 - 189
+  @prime 115792089237316195423570985008687907853269984665640564039457584007913129639747
+
+  def get_prime, do: @prime
+
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ESSS.hello()
-      :world
-
+  Returns a random number from the range 1 <= n <= @prime-1 inclusive
   """
-  def hello do
-    :world
+  def random_number() do
+    :rand.uniform(@prime-1)
+  end
+
+  @doc """
+  Computes the multiplicative inverse of the number on the field @prime.
+  """
+  def modinv(a, m) do
+    {g, x, _y} = Integer.extended_gcd(a, m)
+    if g != 1 do
+      raise("modular inverse does not exist")
+    else
+      Integer.mod(x, m)
+    end
+  end
+
+  @doc """
+  Returns the Int number base10 in Hex representation.
+  note: this is not a string representation; the Hex output is exactly 256 bits long.
+  """
+  def to_hex(number) do
+    hex_string = Integer.to_string(number, 16) |> String.pad_leading(64, "0")
+    hex_string
+  end
+
+  @doc """
+  Returns the number Hex in base 10 Int representation.
+  note: this is not coming from a string representation; the Hex input is
+  exactly 256 bits long, and the output is an arbitrary size base 10 integer.
+  """
+  def from_hex(hex) do
+    String.to_integer(hex, 16)
   end
 end
--- test/esss_test.exs
+++ test/esss_test.exs
@@ -1,8 +1,30 @@
 defmodule ESSSTest do
   use ExUnit.Case
-  doctest ESSS
+  # doctest ESSS
 
-  test "greets the world" do
-    assert ESSS.hello() == :world
+  test "Gen random number" do
+    n = ESSS.random_number()
+    # IO.puts(n)
+    assert 0 < n
+    assert n < ESSS.get_prime()
+  end
+
+  test "Get modinv" do
+    x = ESSS.modinv(2, 7)
+    # IO.puts(x)
+    assert x == 4
+  end
+
+  test "Get hex string of integer" do
+    hex = ESSS.to_hex(255)
+    # IO.puts(hex)
+    assert hex == "00000000000000000000000000000000000000000000000000000000000000FF"
+    assert String.length(hex) == 64
+  end
+
+  test "Get integer from hex string" do
+    i = ESSS.from_hex("00000000000000000000000000000000000000000000000000000000000000FF")
+    # IO.puts(i)
+    assert i == 255
   end
 end
