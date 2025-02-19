86426069e13e16c68708634a92949035cd91d8ca
congnghia0609 <congnghia0609@gmail.com>
2025-02-19 09:21:05 +0700

    update file mix.exs and add file changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## [0.1.0] - Feb 19, 2025.
+
+* First release. See file README.md.  
--- mix.exs
+++ mix.exs
@@ -6,15 +6,21 @@ defmodule SSS.MixProject do
       app: :sss,
       version: "0.1.0",
       elixir: "~> 1.18",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "sss",
+      source_url: "https://github.com/congnghia0609/sss"
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      # extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
@@ -23,6 +29,22 @@ defmodule SSS.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "sss is an implement of Shamir's Secret Sharing Algorithm 256-bits in Elixir."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "sss",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/congnghia0609/sss"}
     ]
   end
 end
