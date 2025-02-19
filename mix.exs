3bc4cc14a936cf303091447a33ff7d6c69c43615
congnghia0609 <congnghia0609@gmail.com>
2025-02-19 09:26:27 +0700

    update file mix.exs

--- mix.exs
+++ mix.exs
@@ -41,8 +41,7 @@ defmodule SSS.MixProject do
       # This option is only needed when you don't want to use the OTP application name
       name: "sss",
       # These are the default files included in the package
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* CHANGELOG* changelog* src),
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG* src),
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/congnghia0609/sss"}
     ]
